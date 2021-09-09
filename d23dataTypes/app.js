a8524e8a209117f8c84e0950b03d571a89b8b936
azamit123 <azamitbelew@gmail.com>
2021-09-08 21:38:36 -0500

    Create app.js

---
+++ d23dataTypes/app.js
@@ -0,0 +1,69 @@
+"use strict";
+/* eslint-disable */
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+// module.exports = { ucFirst, getMaxSubSum, truncate , camelize, checkSpam,extractCurrencyValue}; //add all of your function names here that you need for the node mocha tests
+
+
+
+function ucFirst(str) {
+    let firstLetter = str.slice(0,1);
+    firstLetter= firstLetter.toUpperCase();
+    return firstLetter + str.slice(1) ;
+  }
+
+  function checkSpam(str) {
+
+     return (str.includes("ViAgRA")|| str.includes("xxxxx"));
+  }
+
+
+  function truncate(str, maxlength) {
+    const ending = "…"
+    if(str.length >= maxlength){
+    return str.slice(0,maxlength-1)+ ending;
+    }else{
+        return str.slice(0,maxlength+1)
+    } 
+
+  }
+
+  function extractCurrencyValue(str){
+      const actualNumber = str.slice(1);
+      const currency = Number(actualNumber);
+      return currency;
+  }
+
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} the total of the maximal subarray
+ 
+ */
+function getMaxSubSum(arr) {
+
+    let sum =0;
+    for(const elements of arr){
+        sum+=elements;
+    }
+    
+
+}
+
+
+function camelize(str) {
+     let words = str.split("-");
+
+    let result = "";
+  for (let i = 0 ; i < words.length ; i++) {
+    let word = words[i];
+    let capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+    result += capitalizedWord;
+  }
+ 
+  return result;
+ 
+  }
+
+  

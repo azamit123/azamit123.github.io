825e11bfec126684a88db4a51257aedb88b1b118
azamit123 <azamitbelew@gmail.com>
2021-09-08 23:43:58 -0500

    Update app.js

--- d23dataTypes/app.js
+++ d23dataTypes/app.js
@@ -45,10 +45,10 @@ function getMaxSubSum(arr) {
 
     let maxSum = 0; 
 for (let i = 0; i < arr.length; i++) { 
-    let sumFixedStart = 0;
+    let sumStart = 0;
 for (let j = i; j < arr.length; j++) {
-sumFixedStart += arr[j];
-maxSum = Math.max(maxSum, sumFixedStart); }
+sumStart += arr[j];
+maxSum = Math.max(maxSum, sumStart); }
 }
 return maxSum; }
     

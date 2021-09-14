1dcb2fa109166557a61cbeeee80c40724b4b854e
azamit123 <azamitbelew@gmail.com>
2021-09-13 22:23:25 -0500

    Create destructure.js

---
+++ d25destructure/destructure.js
@@ -0,0 +1,27 @@
+"use strict";
+/* eslint-disable */
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+// module.exports = { topSalary, getDateAgo }; //add all of your function names here that you need for the node mocha tests
+
+
+function topSalary(salaries) {
+let max =0;
+let maxName = null;
+for(const [name, salary] of Object.entries(salaries)) {
+if(salaries === {}) {
+return null;
+ }else
+if(max < salary) {
+max = salary;
+maxName = name;
+ }
+ }
+return maxName; 
+}
+
+
+  function getDateAgo(date, days) {
+
+  }
+
+ 
